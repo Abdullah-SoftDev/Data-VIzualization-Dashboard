e0e1b8fed60fa45d522de174f5376c57f5eb9d93
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-11-30 22:16:51 +0530

    update the auth process while data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ import BarChart from "./components/bar-chart";
 
 const HomePage = async () => {
   // Authenticate with Google Sheets API
-  const auth = await google.auth.getClient({
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
+    },
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
 
